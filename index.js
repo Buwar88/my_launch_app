f892fc956124839039cc10961e78b31e36ac2971
Buwar88 <buwar_emerica@hotmail.com>
2022-04-20 23:23:43 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const irving = new MissionCommander("Irving")
+console.log(irving.name)
+
+const adan = new MissionCommander("Adan")
+console.log(adan.name)
+
+
